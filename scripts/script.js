27265730d4880cea9746a593805c1464a7fc5503
Igor Pova <igorp2000@hotmail.com>
2024-03-26 17:32:52 -0300

    implementei o codigo das aulas 8, 9 e 10

--- scripts/script.js
+++ scripts/script.js
@@ -36,9 +36,50 @@ async function loadCharacters(url) {
             characterName.className = "character-name";
             characterName.innerText = `${character.name}`;
 
-            mainContent.appendChild(card);
-            card.appendChild(characterNameBg);
             characterNameBg.appendChild(characterName);
+            card.appendChild(characterNameBg);
+
+            card.onclick = () => {
+                const modal = document.getElementById("modal");
+                modal.style.visibility = "visible";
+
+                const modalContent = document.getElementById("modalContent");
+                modalContent.innerHTML = '';
+
+                const characterImage = document.createElement("div");
+                characterImage.style.backgroundImage = 
+                `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g, "")}.jpg')`;
+                characterImage.className = "characterImage";
+
+                const name = document.createElement("span");
+                name.className = "cahracterDetails";
+                name.innerText = `Nome: ${character.name}`;
+
+                const characterHeight = document.createElement("span");
+                characterHeight.className = "cahracterDetails";
+                characterHeight.innerText = `Altura: ${convertHeight(character.height)}`;
+
+                const mass = document.createElement("span");
+                mass.className = "cahracterDetails";
+                mass.innerText = `Peso: ${convertMass(character.mass)}`;
+
+                const eyeColor = document.createElement("span");
+                eyeColor.className = "cahracterDetails";
+                eyeColor.innerText = `Cor dos olhos: ${convertEyeColor(character.eye_color)}`;
+
+                const birthYear = document.createElement("span");
+                birthYear.className = "cahracterDetails";
+                birthYear.innerText = `Nascimento: ${convertBirthYear(character.birth_year)}`;
+
+                modalContent.appendChild(characterImage);
+                modalContent.appendChild(name);
+                modalContent.appendChild(characterHeight);
+                modalContent.appendChild(mass);
+                modalContent.appendChild(eyeColor);
+                modalContent.appendChild(birthYear);
+            }
+
+            mainContent.appendChild(card);
         });
 
         const nextButton = document.getElementById('button-next');
@@ -56,4 +97,80 @@ async function loadCharacters(url) {
         alert('Erro ao carregar os personagens');
         console.log(error);
     }
+}
+
+async function loadNextPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl);
+        const responseJson = await response.json();
+
+        await loadCharacters(responseJson.next);
+
+    } catch (error) {
+        console.log(error);
+        alert("Erro ao carregar a pagina");
+    }
+}
+
+async function loadPreviousPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl);
+        const responseJson = await response.json();
+
+        await loadCharacters(responseJson.previous);
+
+    } catch (error) {
+        console.log(error);
+        alert("Erro ao carregar a pagina");
+    }
+}
+
+function hideModal() {
+    const modal = document.getElementById("modal");
+    modal.style.visibility = "hidden";
+}
+
+function convertEyeColor(eyeColor) {
+    const cores = {
+        blue: "azul", 
+        brown: "castanho", 
+        green: "verde", 
+        yellow: "amarelo", 
+        black: "preto", 
+        pink: "rosa", 
+        red: "vermelho", 
+        orange: "laranja", 
+        hazel: "avela", 
+        unknown: "desconhecida"
+    };
+
+    return cores[eyeColor.toLowerCase()] || eyeColor;
+}
+
+function convertHeight(height) {
+    if (height === "unknown") {
+        return "desconhecida";
+    }
+
+    return (height / 100).toFixed(2);
+}
+
+function convertMass(mass) {
+    if (mass === "unknown") {
+        return "desconhecido";
+    }
+
+    return `${mass} kg`
+}
+
+function convertBirthYear(birthYear) {
+    if (birthYear === "unknown") {
+        return "desconhecido";
+    }
+
+    return birthYear
 }
